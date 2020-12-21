a0b05ea3b2a8785d97e7af90dcb223479dde82b8
Mattéo Delabre <spam@delab.re>
2020-12-21 22:05:06 +0100

    AoC day 21

---
+++ 2020/21-1.py
@@ -0,0 +1,58 @@
+import re
+
+line_regex = re.compile(r'(.+) \(contains (.+)\)')
+rules = []
+all_ingreds = set()
+all_allergs = set()
+
+try:
+    while True:
+        ingreds_str, allergs_str = line_regex.match(input()).groups()
+        ingreds = ingreds_str.split()
+        allergs = allergs_str.split(', ')
+
+        all_ingreds.update(ingreds)
+        all_allergs.update(allergs)
+
+        rules.append((ingreds, allergs))
+except EOFError:
+    pass
+
+def check_rules(assign_allerg):
+    for ingreds, allergs in rules:
+        for allerg in allergs:
+            if allerg in assign_allerg and assign_allerg[allerg] not in ingreds:
+                return False
+
+    return True
+
+def search(assign_allerg, next_allergs, remain_ingred):
+    if not next_allergs:
+        return [assign_allerg]
+
+    allerg = next_allergs[0]
+    solutions = []
+
+    for ingred in remain_ingred:
+        assign_allerg[allerg] = ingred
+
+        if check_rules(assign_allerg):
+            solutions += search(
+                assign_allerg.copy(),
+                next_allergs[1:],
+                remain_ingred - set((ingred,)))
+
+    return solutions
+
+
+solutions = search({}, list(all_allergs), all_ingreds)
+assert len(solutions) == 1
+solution = solutions[0]
+
+safe_ingreds = all_ingreds - set(solution.values())
+print(sum([
+    1
+    for ingreds, allergs in rules
+    for ingred in safe_ingreds
+    if ingred in ingreds
+]))
