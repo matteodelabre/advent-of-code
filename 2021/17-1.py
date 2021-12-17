653a8a6c4fb5f193831d7cc93a1a7c7667ec1e13
Mattéo Delabre <spam@delab.re>
2021-12-17 00:29:47 -0500

    AoC 2021 day 17

---
+++ 2021/17-1.py
@@ -0,0 +1,33 @@
+from math import inf
+import re
+
+regex = r"target area: x=([\d-]+)..([\d-]+), y=([\d-]+)..([\d-]+)"
+target = tuple(map(int, re.match(regex, input()).groups()))
+
+def works(vx, vy):
+    x, y = 0, 0
+    max_y = -inf
+
+    while x <= target[1] and y >= target[2]:
+        x += vx
+        y += vy
+        max_y = max(y, max_y)
+
+        if vx != 0:
+            vx -= vx // abs(vx)
+
+        vy -= 1
+
+        if target[0] <= x <= target[1] and target[2] <= y <= target[3]:
+            return max_y
+
+    return -inf
+
+x_amp = max(map(abs, target[:2]))
+y_amp = max(map(abs, target[2:]))
+
+print(max(
+    works(vx, vy)
+    for vx in range(1, x_amp + 1)
+    for vy in range(-y_amp, y_amp + 1)
+))
