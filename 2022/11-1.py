bc84b513ea78da036420e0a1d433f552cb5dd5f3
Mattéo Delabre <git.matteo@delab.re>
2022-12-11 00:39:13 -0500

    AoC 2022 day 11

---
+++ 2022/11-1.py
@@ -0,0 +1,42 @@
+from collections import Counter
+from math import prod
+import re
+
+digit = re.compile("\d+")
+
+# Parse input
+items = {}
+instrs = {}
+
+for data in open(0).read().split("\n\n"):
+    lines = data.splitlines()
+    monkey = int(digit.search(lines[0]).group(0))
+
+    items[monkey] = list(map(int, digit.findall(lines[1])))
+    operation = lines[2].split(" = ")[1]
+    instrs[monkey] = {
+        "operation": eval("lambda old: " + operation),
+        "test": int(digit.search(lines[3]).group(0)),
+        "true": int(digit.search(lines[4]).group(0)),
+        "false": int(digit.search(lines[5]).group(0)),
+    }
+
+# Simulate monkey dance
+activity = Counter()
+
+for _ in range(20):
+    for monkey, instr in instrs.items():
+        for worry in items[monkey]:
+            worry = instr["operation"](worry)
+            worry //= 3
+
+            if worry % instr["test"] == 0:
+                items[instr["true"]].append(worry)
+            else:
+                items[instr["false"]].append(worry)
+
+            activity[monkey] += 1
+
+        items[monkey] = []
+
+print(prod(value for monkey, value in activity.most_common(2)))
